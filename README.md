09ffc7384fb2557f32a0a7c27062f740d3f9cac4
Alex-xd <zhangboyuan_xd@qq.com>
2016-10-29 02:39:21 +0800

    v1.1.0

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 ## jquery.pagination.js
 
-[![npm](https://img.shields.io/badge/npm-1.0.0-blue.svg)](https://www.npmjs.com/package/upload-preview)
+[![npm](https://img.shields.io/badge/npm-1.1.0-blue.svg)](https://www.npmjs.com/package/upload-preview)
 [![lisence](https://img.shields.io/badge/LISENCE-MIT-green.svg)](https://github.com/Alex-xd/preview-upload)
 
-> A light and very easy use jq-plugin for pagination. 
+> A light 、highly customizability and very easy use jquery plugin for pagination ^ ^
 
-使用超简单的jQuery分页插件，只提供原始功能，根据自己的需求随意添加样式！代码简洁高效。
+![preview](http://o6x2vif88.bkt.clouddn.com/Screen%20Shot%202016-10-29%20at%2001.04.52.png)
+
+史上易用性最强的分页插件，压缩后仅为
+
+- 多条件动态分页实例
+- 提供多种API接口以供扩展功能
+- 自定义回调函数支持
+- 可自定义页码显示数量
+- 可自定义按钮名称
+- 可选的省略号、首页尾页按钮
+- 样式完全可自定义化（默认不加载任何样式，干净利落，根据需求添加样式）
+- 所有元素类名可自定义
+
 
 ## Usage
 
@@ -13,15 +25,17 @@
 
 ```html
 <script src="jquery.min.js"></script>
-<script src="jquery.pagination.js"></script>
+<script src="jquery.pagination.min.js"></script>
 
 <div id="pager"></div>
 
 <script>
-$('#pager').paginate({
-    pageIndex: 0, 		//初始页（0为第一页）
-    totlePageCount: 16, //总页数
-    maxBtnCount: 5 		//按钮显示个数
+//这里列出了4个常用的设置，不设置就采用默认配置
+var pager = $('#pager').paginate({
+        pageIndex: 0, //当前页数
+        totlePageCount: 16, //总页数
+        maxBtnCount: 7, //按钮数量最多有
+        styleURL: './defaultStyles/style1.css' //样式路径 默认无样式
 });
 </script>
 ```
@@ -33,9 +47,56 @@ $('#pager').paginate({
 
 then use `npm test` if u want.
 
-## More
+## You want More
 
-正在开发中，欢迎prissuestarforkwatchwhatever
+#### 可选配置和API
+
+|名称|类型|作用|默认值|
+|--- |---|---|---|
+|pageIndex|Number|当前页码|0|
+|totlePageCount|Number|总页码|16|
+|maxBtnCount|Number|最大显示按钮数量|7|
+|hasDots|Boolean|是否显示省略号|true|
+|hasFirstLast|Boolean|是否开启首尾页跳转功能|true|
+|styleURL|String|自定义样式路径（默认不加载样式）|空字符串|
+|btnClass|String|数字按钮类名|'pager-numbtn'|
+|pageTurnClass|String|翻页按钮类名|'pager-turnbtn'|
+|firstLastClass|String|首页尾页按钮类名|'pager-flbtn'|
+|prevPageName|String|"上一页"标签文字|'prev'|
+|nextPageName|String|"下一页"标签文字|'next'|
+|firstPageName|String|"首页"标签文字|'First Page'|
+|lastPageName|String|"尾页"标签文字|'Last Page'|
+
+<br>
+## API
+先创建实例
+
+```javascript
+var pager = $('#pager').paginate();
+```
+
+
+### pager.set(option, value)
+更改配置属性
+
+@param {String} option 要设置的配置属性
+
+@param {String}{Number}{Boolean} value 要设置的值
+
+
+### pager.getPageIndex()
+
+获取当前页
+
+@param  {Number} pageIndex 要跳转到的页码（值为实际页码减一，如：跳到第3页就传入2）
+
+### pager.turnToPage(pageIndex)
+
+跳转页码
+
+@param  {Number} pageIndex 要跳转到的页码（值为实际页码减一，如：跳到第3页就传入2）
+
+<br>
 
 ## LISENCE
 MIT
--- jquery.pagination.js
+++ jquery.pagination.js
@@ -2,7 +2,7 @@
  * jquery.pagination.js 
  *
  * @author Alexxd
- * @version v1.0.0
+ * @version v1.1.0
  * @license MIT - http://www.opensource.org/licenses/mit-license.php
  *
  * For usage and examples, visit:
@@ -10,85 +10,165 @@
  *
  * Copyright (c) 2016, Alexxd
  */
-;(function($) {
+;
+(function($) {
 
     'use strict'
 
     var defaults = {
         pageIndex: 0, //当前页数
         totlePageCount: 16, //总页数
-        maxBtnCount: 8 //按钮最多有
-    }
+        maxBtnCount: 7, //按钮数量最多有
+        hasDots: true, //是否显示省略号
+        hasFirstLast: true, //是否开启首尾页跳转功能
+        btnClass: 'pager-numbtn', //数字按钮类名
+        pageTurnClass: 'pager-turnbtn', //翻页按钮类名
+        firstLastClass: 'pager-flbtn', //首页尾页按钮类名
+        prevPageName: 'prev', //“上一页”标签文字
+        nextPageName: 'next', //“下一页”标签文字
+        firstPageName: 'First Page',
+        lastPageName: 'Last Page',
+        styleURL: '' //自定义样式路径 默认无样式
+    };
 
-    function Pager($elem, options) {
-        //this是pager实例对象
+    function Pager($elem, options, callback) {
         this.$elem = $elem;
         this.options = $.extend({}, defaults, options);
         this.init();
+        if (callback) {
+            callback();
+        }
     }
     Pager.prototype = {
-            constructor: Pager,
-            init: function() {
-                //this是pager实例对象
-                this._render();
-                this._bind();
-            },
-            _render: function() {
-                var options = this.options,
-                    html = [];
+        constructor: Pager,
+        init: function() {
+            var self = this;
 
-                // 如果按钮数比总页数还多，就按总页数渲染按钮
-                options.maxBtnCount = options.maxBtnCount < options.totlePageCount ? options.maxBtnCount : options.totlePageCount;
+            //1.加载样式
+            if (self.options.btnClass !== 'pager-numbtn') {
+                console.error('(jquery.pagination) Error:样式加载失败！  Reason:按钮类名不正确,请将 btnClass 属性设为 \'pager-numbtn\' 或不设置');
+            } else if (self.options.pageTurnClass !== 'pager-turnbtn') {
+                console.error('(jquery.pagination) Error:样式加载失败！  Reason:按钮类名不正确,请将 pageTurnClass 属性设为 \'pager-turnbtn\' 或不设置');
+            } else if (self.options.firstLastClass !== 'pager-flbtn') {
+                console.error('(jquery.pagination) Error:样式加载失败！  Reason:按钮类名不正确,请将 firstLastClass 属性设为 \'pager-flturn\' 或不设置');
+            } else if ($('link').toArray().some(function(elem, index, array) {
+                    //判断页面是否存在同名样式文件
+                    return elem.getAttribute('href') === self.options.styleURL;
+                })) {
+                console.warn('(jquery.pagination) Warn: ' + '"' + self.options.styleURL + '"' + '加载失败! ' + '您的页面已存在一个' + '"' + self.options.styleURL.slice(self.options.styleURL.lastIndexOf('/') + 1) + '"' + '文件!');
+            } else if (self.options.styleURL) {
+                $('head').append('<link rel="stylesheet" href="' + self.options.styleURL + '">');
+            } else {
+                console.error('Error: Something Wrong. Maybe you can put an issue at ' + "https://github.com/Alex-xd/jquery-pagination/issues" + '')
+            }
 
-                var frame = {
-                    left: 0,
-                    right: 0
-                };
-                //判断当前页码左侧和右侧是否触边（把frame想象成一个左右移动的框架，只在frame的范围内渲染对应的按钮）
-                if ((options.pageIndex - Math.floor(options.maxBtnCount / 2)) <= 0) {
-                    frame.left = 0;
-                    frame.right = frame.left + options.maxBtnCount - 1;
-                } else if ((options.pageIndex + Math.floor(options.maxBtnCount / 2)) >= options.totlePageCount - 1) {
-                    frame.right = options.totlePageCount - 1;
-                    frame.left = frame.right - options.maxBtnCount + 1;
+            //2.渲染HTML
+            self._render();
+
+            //3.绑定事件
+            self._bind();
+        },
+        _render: function() {
+            var options = this.options,
+                html = [];
+
+            //若总页数比最大数字按钮数还小，则令数字按钮数等于总页数
+            this.options.maxBtnCount = this.options.maxBtnCount < this.options.totlePageCount ? this.options.maxBtnCount : this.options.totlePageCount;
+
+            //定义按钮渲染边界
+            var frame = {
+                left: 0,
+                right: 0
+            };
+            //判断当前页码左侧和右侧是否触边（把frame想象成一个左右移动的框架，只在frame的范围内渲染对应的按钮）
+            if ((options.pageIndex - Math.floor(options.maxBtnCount / 2)) <= 0) {
+                frame.left = 0;
+                frame.right = frame.left + options.maxBtnCount - 1;
+            } else if ((options.pageIndex + Math.floor(options.maxBtnCount / 2)) >= options.totlePageCount - 1) {
+                frame.right = options.totlePageCount - 1;
+                frame.left = frame.right - options.maxBtnCount + 1;
+            } else {
+                frame.left = options.pageIndex - Math.floor(options.maxBtnCount / 2);
+                //奇数不减一  偶数减一
+                frame.right = options.pageIndex + Math.ceil(options.maxBtnCount / 2) - 1;
+            }
+
+            /**
+             * 渲染模板函数
+             * @param  {Array} className 类名
+             * @param  {Number} data-page 属性页码
+             * @param  {String} content 内容
+             * @return {String} 
+             */
+            function __template(className, data_page, content) {
+                return '<a href=javascript:; class="' + className.join(' ') + '" data-page="' + data_page + '">' + (content) + '</a> ';
+            }
+            //渲染数字按钮
+            for (var i = frame.left; i <= frame.right; i++) {
+                if (i === options.pageIndex) {
+                    html.push(__template([options.btnClass, 'on'], i, i + 1));
                 } else {
-                    frame.left = options.pageIndex - Math.floor(options.maxBtnCount / 2);
-                    //奇数不减一  偶数减一
-                    frame.right = options.pageIndex + Math.ceil(options.maxBtnCount / 2) - 1;
+                    html.push(__template([options.btnClass], i, i + 1));
                 }
+            }
 
-                //添加按钮
-                for (var i = frame.left; i <= frame.right; i++) {
-                    if (i === options.pageIndex) {
-                        html.push('<a href=javascript:; data-page="' + i + '" style="background-color:#20caab;">' + (i + 1) + '</a> ');
-                    } else {
-                        html.push('<a href=javascript:; data-page="' + i + '">' + (i + 1) + '</a> ');
-                    }
-                }
-                //添加省略号
+            //渲染省略号
+            if (options.hasDots) {
                 if (frame.left > 0) {
-                    html.unshift('<span data-page="' + frame.left + '">...</span>');
+                    html.unshift(__template([options.btnClass], frame.left, '...'));
                 }
                 if (frame.right < options.totlePageCount - 1) {
-                    html.push('<span data-page="' + frame.right + '">...</span>');
+                    html.push(__template([options.btnClass], frame.right, '...'));
                 }
-                //添加翻页
-                html.unshift('<a href=javascript:; data-page="' + (Math.max(0, options.pageIndex - 1)) + '">上一页</a> ');
-                html.push(' <a href=javascript:; data-page="' + (Math.min(options.totlePageCount, options.pageIndex + 1)) + '">下一页</a><br><br>');
-
-                this.$elem.html(html);
-            },
-            _bind: function() {
-                var self = this;
-
-                self.$elem.on('click', function(e) {
-                    self.options.pageIndex = parseInt(e.target.getAttribute('data-page'), 10);
-                    self._render();
-                });
             }
+
+            //渲染翻页按钮
+            html.unshift(__template([options.pageTurnClass], Math.max(0, options.pageIndex - 1), options.prevPageName));
+            html.push(__template([options.pageTurnClass], Math.min(options.totlePageCount, options.pageIndex + 1), options.nextPageName));
+
+            //渲染首尾页跳转按钮
+            if (options.hasFirstLast) {
+                html.unshift(__template([options.firstLastClass], 0, options.firstPageName));
+                html.push(__template([options.firstLastClass], options.totlePageCount - 1, options.lastPageName));
+            }
+
+            this.$elem.html(html);
+        },
+        _bind: function() {
+            var self = this;
+            self.$elem.on('click', 'a', function(e) {
+                self.options.pageIndex = parseInt(e.target.getAttribute('data-page'), 10);
+                self._render();
+            });
+        },
+        /**
+         * 更改配置属性
+         * @param {String} option 要设置的配置属性
+         * @param {String}{Number}{Boolean} value 要设置的值
+         */
+        set: function(option, value) {
+            this.options[option] = value;
+            this._updateOptions();
+            this._render();
+        },
+        /**
+         * 获取当前页
+         * @return {Number} 当前页码（值为实际页码减一  如：第1页返回）
+         */
+        getPageIndex: function() {
+            return this.options.pageIndex;
+        },
+        /**
+         * 跳转页码
+         * @param  {Number} pageIndex 要跳转到的页码（值为实际页码减一，如：跳到第3页就传入2）
+         */
+        turnToPage: function(pageIndex) {
+            this.options.pageIndex = pageIndex;
+            this._render();
         }
-        //TODO:1、再添加一些API（获取当前页，获取总页数，回调函数等） 2、添加几个样式供用户选择
-    $.fn.paginate = function(options) {
-        return new Pager($(this), options);
+    }
+
+    $.fn.paginate = function(options, callback) {
+        return new Pager($(this), options, callback);
     }
 }(jQuery))
